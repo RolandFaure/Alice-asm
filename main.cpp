cac112e27ad29d36703e0b26c4d832eb16071ea5
Roland Faure <roland.faure@irisa.fr>
2025-03-28 11:09:29 +0100

    removed bug in fastq_to_fasta

--- main.cpp
+++ main.cpp
@@ -36,8 +36,8 @@ using std::set;
 #define GREEN_TEXT "\033[1;32m"
 #define RESET_TEXT "\033[0m"
 
-string version = "0.6.30";
-string date = "2024-03-03";
+string version = "0.6.31";
+string date = "2024-03-28";
 string author = "Roland Faure";
 
 void check_dependencies(string assembler, string path_bcalm, string path_hifiasm, string path_spades, string path_minia, string path_raven, string path_to_flye, string path_minimap, string path_miniasm, string path_minipolish, string path_megahit, string path_fastg2gfa,
